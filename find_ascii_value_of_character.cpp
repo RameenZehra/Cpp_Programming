2cf051c3cc939e05ebaa45efdc9d222ec6545c48
RameenZehra <rameenzaidi786@gmail.com>
2023-12-03 01:41:49 +0500

    Program to find ASCII value of a character

---
+++ find_ascii_value_of_character.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char ch;
+    cout << "Enter any character:" << endl;
+    cin >> ch;
+    cout<<"ASCII value of " << ch << " is:" << int(ch) << endl;
+
+    return 0;
+}
